248fde895b83f4d3abed7ae122a7f6b2116e0200
yetoneya <ezhi92@mail.ru>
2021-01-06 18:07:20 +0500

    terraform-2

---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,16 @@
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+
+}
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+
+}
+variable subnet_id {
+  description = "Subnet"
+  default =""
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = ""
+}
