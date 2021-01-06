88331e4821141913dc3438d886552fe028d4bbe4
yetoneya <ezhi92@mail.ru>
2021-01-06 18:25:21 +0500

    terraform-2

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "external_ip_address_app" {
-  value = [ for v in yandex_compute_instance.app : v.network_interface.0.nat_ip_address ]
-}
-
-output "external_ip_address_lb" {
-  value = yandex_lb_network_load_balancer.lb.listener.*.external_address_spec
-}
