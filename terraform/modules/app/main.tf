7e78dac3c555806b5416c8f13e8c2f7e1a0d3c9d
yetoneya <ezhi92@mail.ru>
2021-01-06 21:22:15 +0500

    add modules

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,47 +1,26 @@
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
-  platform_id               = "standard-v2"
+
   labels = {
     tags = "reddit-app"
   }
-
   resources {
-    cores  = 2
+    cores  = 1
     memory = 2
-    core_fraction = 20
   }
 
   boot_disk {
     initialize_params {
       image_id = var.app_disk_image
     }
-
   }
+
   network_interface {
     subnet_id = var.subnet_id
-    nat       = true
+    nat = true
   }
 
-  #  zone                     = var.zone_app
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
-
-  connection {
-    type  = "ssh"
-    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "file" {
-    source      = "../files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "../files/deploy.sh"
-  }
-
 }
--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,53 +1,11 @@
 resource "yandex_compute_instance" "db" {
   name = "reddit-db"
-  platform_id               = "standard-v2"
   labels = {
     tags = "reddit-db"
   }
 
   resources {
-    cores  = 2
-    memory = 2
-    core_fraction = 20
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = var.db_disk_image
-    }
-
-  }
-  network_interface {
-    subnet_id = var.subnet_id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type  = "ssh"
-    host  = yandex_compute_instance.db.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "remote-exec" {
-    script = "../files/install_mongodb.sh"
-  }
-
-}
-resource "yandex_compute_instance" "db" {
-  name = "reddit-db"
-  labels = {
-    tags = "reddit-db"
-  }
-
-  resources {
-    core_fraction = 5
-    cores  = 2
+    cores  = 1
     memory = 2
   }
 
@@ -64,20 +22,5 @@ resource "yandex_compute_instance" "db" {
 
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-  connection {
-    type  = "ssh"
-    host  = yandex_compute_instance.db.network_interface[0].nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    # путь до приватного ключа
-    private_key = file(var.private_key_path)
-  }
-  provisioner "file" {
-    content     = templatefile("${path.module}/files/mongod.conf", { db_ipaddr = yandex_compute_instance.db.network_interface.0.ip_address})
-    destination = "/tmp/mongod.conf"
-  }
-  provisioner "remote-exec" {
-    script = "${path.module}/files/deploy.sh"
   }
 }
