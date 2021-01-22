46600bcf26330dbc852d93fbad83e6062e087a8d
elena <ezhi92@mail.ru>
2021-01-22 20:56:25 +0500

    ansible-3

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,8 +1,8 @@
 resource "yandex_compute_instance" "app" {
-  name = "reddit-app-a"
+  name = "reddit-app"
 
   labels = {
-    tags = "reddit-app-a"
+    tags = "reddit-app"
   }
 
   resources {
@@ -19,6 +19,7 @@ resource "yandex_compute_instance" "app" {
   network_interface {
     subnet_id = var.subnet_id
     nat = true
+
   }
 
   metadata = {
@@ -60,3 +61,40 @@ resource "null_resource" "app" {
     script = "${path.module}/files/deploy.sh"
   }
 }
+
+/*resource "yandex_vpc_network" "lab-net" {
+  name = "lab-network"
+}
+
+resource "yandex_vpc_security_group" "group1" {
+  name        = "My security group"
+  description = "description for my security group"
+  network_id  = "${yandex_vpc_network.lab-net.id}"
+
+  labels = {
+    my-label = "my-label-value"
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "rule1 description"
+    v4_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+    port           = 8080
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "rule2 description"
+    v4_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+    from_port      = 8090
+    to_port        = 8099
+  }
+
+  egress {
+    protocol       = "UDP"
+    description    = "rule3 description"
+    v4_cidr_blocks = ["10.0.1.0/24"]
+    from_port      = 8090
+    to_port        = 8099
+  }
+}*/
